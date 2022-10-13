dd186c589abe515b0ddd792175941a23f48095a0
alejandro1345 <a.albarracin@hotmail.com>
2022-10-13 20:40:26 -0300

    segundo commit

---
+++ sistema/libreria/views.py
@@ -0,0 +1,20 @@
+import http
+from http.client import HTTP_PORT
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def inicio(request):
+     return render(request, 'paginas/inicio.html')
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    return render(request,'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')
+
+# Create your views here.
